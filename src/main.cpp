cf30694aacff41c592849aac857b64a86229887c
Jessica Alder <jessalder1989@gmail.com>
2024-02-20 23:25:26 -0800

    Bug repro code for frame time issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <raylib.h>
 #include <raymath.h>
 #include "view.h"
@@ -7,11 +8,24 @@ int main()
     // Initial window viewport setup. Check View for scaling
     View Viewport;                          
     InitWindow(Viewport.Width, Viewport.Height, "A New Window");
-    SetTargetFPS(120);
 
+    float deltaTime = 0.f;
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        deltaTime = GetFrameTime();
+
+        if(IsKeyDown(KEY_A))
+        {
+            if(deltaTime > 0.002)
+            {
+                std::cout << "Frame Time Spike: " << deltaTime << std::endl;
+            } else 
+            {
+                std::cout << "Frame Time: " << deltaTime << std::endl;
+            }
+        }
+
         EndDrawing();
     }
     
